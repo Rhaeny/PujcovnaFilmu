8c057f85a59104a7de2f89d7a464ec58ae8e3318
Dusan <Dusan@DESKTOP-ACCK73A>
2016-12-10 21:09:37 +0100

    21:09 10.12.2016

---
+++ Services/FilmService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using ORM.DAO;
+using ORM.DTO;
+
+namespace Services
+{
+    public class FilmService : IFilmService
+    {
+        public void DoWork()
+        {
+        }
+
+        public int Insert(Film film, Database pDb = null)
+        {
+            return FilmTable.Insert(film, pDb);
+        }
+
+        public int Update(Film film, Database pDb = null)
+        {
+            return FilmTable.Update(film, pDb);
+        }
+
+        public Collection<Film> Select(Database pDb = null)
+        {
+            return FilmTable.Select(pDb);
+        }
+    }
+}
---
+++ Services/IFilmService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.ServiceModel;
+using ORM.DAO;
+using ORM.DTO;
+
+namespace Services
+{
+    [ServiceContract]
+    public interface IFilmService
+    {
+        [OperationContract]
+        void DoWork();
+        int Insert(Film film, Database pDb = null);
+        int Update(Film film, Database pDb = null);
+        Collection<Film> Select(Database pDb = null);
+    }
+}
